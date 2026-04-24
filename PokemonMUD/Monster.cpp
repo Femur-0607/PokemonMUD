6e428d7c4b308e296bdd5c8acc38c645b5039d3d
YangJiseop <vastu54321@gmail.com>
2026-04-24 18:36:30 +0900

    1

---
+++ PokemonMUD/Monster.cpp
@@ -0,0 +1,21 @@
+﻿#include "Monster.h"
+#include<string>
+
+using namespace std;
+
+Monster::Monster(const string &name, int baseHp, int attackPower, int defense, int speed, int level = 1)
+: name(name), hp(hp), maxHp(maxHp), attackPower(attackPower), defense(defense), speed(speed), level(level)
+{
+    maxHp = baseHp * level * 2;
+    hp = maxHp;
+    attackPower = baseAttack * level * 1.5;
+    defense = baseDefense * level * 1.5;
+    speed = baseSpeed * level * 1.5;
+}
+
+Monster::~Monster(){}
+
+string Monster::getName() const
+{
+    return name;
+}
---
+++ PokemonMUD/Monster.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+#include<string>
+
+using namespace std;
+
+class Monster
+{
+protected:
+    string name;
+    int hp, baseHp, maxHp;
+    int attackPower;
+    int defense;
+    int speed;
+    int level;
+public:
+    Monster(const string& name, int hp, int maxHP, int attackPower, int defense, int speed, int level);
+    virtual ~Monster();
+    
+    string getName() const;
+    virtual void Attack(Monster* target);
+    void TakeDamage(int damage);
+    bool IsDead();
+};
